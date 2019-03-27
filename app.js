be0b1e1e08e5b4e618f775419b239ad369738e78
wangTodd <13161282746@163.com>
2019-03-27 17:20:58 +0800

    添加允许跨域

--- app.js
+++ app.js
@@ -5,6 +5,17 @@ const fs = require('fs');
 const port = 8088;
 
 let  app = express();
+// 允许跨域
+app.all('*', function(req, res, next) {
+  　　  res.header("Access-Control-Allow-Origin", "*"); // 表示任意的源
+ 　　　　// res.header("Access-Control-Allow-Origin", "http://www.wtapi.wang"); // 只有这个网址
+ 　　　　res.header("Access-Control-Allow-Headers", "Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
+ 　　　　res.header("Access-Control-Allow-Methods","PUT,POST,GET,DELETE,OPTIONS");
+ 　　　　res.header("X-Powered-By",'unknown')
+ 　　　　res.header("Content-Type", "application/x-www-form-urlencoded; charset=UTF-8");
+ 　　　　next();
+ });
+
 
 app.set('port', port);
 // index.html, index.js放在static文件夹中
